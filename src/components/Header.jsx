7b049f21d6b83c3a7c2b4f8a4b2fd1ead818cf21
bk <rnjsqhrud10@gmail.com>
2022-05-06 15:00:09 +0900

    feat : practice contextAPI

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+export default function Header() {
+  return (
+    <header>
+      <div className="header__container">
+        <div className="title">Awesome Prototypes in Shop</div>
+        <div className="subtitle">
+          Check out what other designers have created using ProtoPie-download
+          these example to learn exactly how they made theirs.
+        </div>
+        <div className="btn__area">
+          <a href="https://www.protopie.io" target="_BLANK" rel="noreferrer">
+            <button>Try ProtoPie Yourself</button>
+          </a>
+        </div>
+      </div>
+    </header>
+  );
+}
